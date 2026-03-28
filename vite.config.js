2af0bc323312d03d1f0054be245cf9f6bccf2bb9
Yohany Cantillo <yohany.cantillo@wyndham.com>
2026-03-28 15:37:07 -0500

    feat: add GitHub Actions workflow for CI/CD and update package.json scripts

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const repo = process.env.GITHUB_REPO;
+
 export default defineConfig({
   plugins: [react()],
+  base: repo ? `/${repo}/` : '/',
 });
